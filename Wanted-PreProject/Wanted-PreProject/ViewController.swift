e9aa1d4b45d631c9dedda75065bd08bc9e7c0612
Kyo <hyosing92@gmail.com>
2023-03-01 16:23:39 +0900

    feat : AutoLayout 설정

--- Wanted-PreProject/Wanted-PreProject/ViewController.swift
+++ Wanted-PreProject/Wanted-PreProject/ViewController.swift
@@ -7,9 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    @IBOutlet weak var thirdImageView: UIImageView!
+    @IBOutlet weak var fourthImageView: UIImageView!
+    @IBOutlet weak var fifthImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func loadImageButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func loadAllImageButtonTapped(_ sender: UIButton) {
+        
     }
 }
