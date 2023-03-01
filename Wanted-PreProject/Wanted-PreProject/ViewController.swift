591c104aaa3bef01cc2b55432604d6ffb746c78e
Kyo <hyosing92@gmail.com>
2023-03-01 17:10:32 +0900

    feat : ViewController 내부 각 버튼 Action 구현

--- Wanted-PreProject/Wanted-PreProject/ViewController.swift
+++ Wanted-PreProject/Wanted-PreProject/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    private let networkService = NetworkService()
+    
     @IBOutlet weak var firstImageView: UIImageView!
     @IBOutlet weak var secondImageView: UIImageView!
     @IBOutlet weak var thirdImageView: UIImageView!
@@ -18,11 +20,52 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    private func selectImageView(tag: Int) -> UIImageView? {
+        let imageView = [firstImageView,
+                         secondImageView,
+                         thirdImageView,
+                         fourthImageView,
+                         fifthImageView]
+            .filter { imageView in
+                
+                if tag == imageView.tag {
+                    return true
+                }
+                return false
+            }
+        
+        return imageView.first
+    }
+}
+
+extension ViewController {
     @IBAction func loadImageButtonTapped(_ sender: UIButton) {
+        guard let request = try? NetworkReqeust(rawValue: sender.tag)?.generateRequest() else {
+            return
+        }
+        
+        let imageView = selectImageView(tag: sender.tag)
+        
+        networkService.request(request) { data in
+            DispatchQueue.main.async {
+                imageView?.image = UIImage(data: data)
+            }
+        }
     }
     
-    
     @IBAction func loadAllImageButtonTapped(_ sender: UIButton) {
-        
+        for tag in 0..<5 {
+            guard let request = try? NetworkReqeust(rawValue: tag)?.generateRequest() else {
+                return
+            }
+            
+            let imageView = self.selectImageView(tag: tag)
+            
+            networkService.request(request) { data in
+                DispatchQueue.main.async {
+                    imageView?.image = UIImage(data: data)
+                }
+            }
+        }
     }
 }
